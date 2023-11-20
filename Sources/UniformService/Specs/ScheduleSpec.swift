7844ba4171d74665e2119888c0c2177c58efbf38
Jordan Kay <jordan@fleuronic.com>
2023-11-20 14:09:44 -0500

    Remove schedule spec

--- Sources/UniformService/Specs/ScheduleSpec.swift
+++
@@ -1,7 +0,0 @@
-// Copyright © Fleuronic LLC. All rights reserved.
-
-public protocol ScheduleSpec {
-	associatedtype ScheduleResult
-
-	func createSchedule(for year: Int) async -> ScheduleResult
-}
