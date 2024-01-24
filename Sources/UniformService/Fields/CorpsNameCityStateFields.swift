54f025598e9b691d679daaf9f9f3d48cc69c10a2
Jordan Kay <jordan@fleuronic.com>
2024-01-23 21:32:09 -0500

    Rename fields

--- Sources/UniformService/Fields/CorpsNameCityStateFields.swift
+++ Sources/UniformService/Fields/CorpsNameCityStateFields.swift
@@ -8,41 +8,21 @@ import struct Schemata.Projection
 import protocol DieselService.CorpsFields
 import protocol Identity.Identifiable
 
-public struct CorpsNameLocationFields {
+public struct CorpsNameCityStateFields {
 	public let id: Corps.ID
 	public let name: String
-	public let location: LocationBaseFields
+	public let city: String
+	public let state: String
 }
 
 // MARK: -
-extension CorpsNameLocationFields: CorpsFields {
+extension CorpsNameCityStateFields: CorpsFields {
 	// MARK: ModelProjection
 	public static let projection = Projection<Corps.Identified, Self>(
 		Self.init,
 		\.id,
 		\.value.name,
-		\.location.id,
 		\.location.value.city,
 		\.location.value.state
 	)
-}
-
-// MARK: -
-private extension CorpsNameLocationFields {
-	init(
-		id: Corps.ID,
-		name: String,
-		locationID: Location.ID,
-		city: String,
-		state: String
-	) {
-		self.id = id
-		self.name = name
-		
-		location = .init(
-			id: locationID,
-			city: city,
-			state: state
-		)
-	}
 }
