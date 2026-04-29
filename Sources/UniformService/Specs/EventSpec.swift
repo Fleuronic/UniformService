f05e5cb6866c5392d097d1fb1700e8f164fe2443
Jordan Kay <jordan@fleuronic.com>
2026-04-29 14:48:55 -0400

    Update event spec

--- Sources/UniformService/Specs/EventSpec.swift
+++ Sources/UniformService/Specs/EventSpec.swift
@@ -20,7 +20,7 @@ public protocol EventSpec {
 
 	associatedtype EventListFields: EventFields
 
-	func listEvents(for year: Int, with corpsRecord: (String) async -> String) async -> EventList
+	func listEvents(for year: Int, with corpsRecord: ((String) async -> String)?) async -> EventList
 	func createEvent(on date: Date, inLocationWith locationID: Location.ID, byCircuitWith circuitID: Circuit.ID?, forShowWith showID: Show.ID?, atVenueWith venueID: Venue.ID?, detailsURL: URL?, scoresURL: URL?) async -> EventCreation
 	func updateEvent(with eventID: DrumKit.Event.ID, on date: Date, inLocationWith locationID: Location.ID, byCircuitWith circuitID: Circuit.ID?, forShowWith showID: Show.ID?, atVenueWith venueID: Venue.ID?, detailsURL: URL?, scoresURL: URL?) async -> EventUpdate
 	func deleteEvents(with ids: [DrumKit.Event.ID]) async -> EventDeletion
